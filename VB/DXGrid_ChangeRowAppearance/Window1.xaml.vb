2ec0c920a57d759ae6a6e63b2f6438c4f8c0a6c3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DXGrid_ChangeRowAppearance/Window1.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Windows
+Imports System.Collections.Generic
+
+Namespace DXGrid_ChangeRowAppearance
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            grid.ItemsSource = Products.GetData()
+        End Sub
+        Public Class Products
+            Public Shared Function GetData() As List(Of Product)
+                Dim data As New List(Of Product)()
+               data.Add(New Product() With {.ProductName = "Chai", .UnitPrice = 18, .UnitsOnOrder = 10})
+                data.Add(New Product() With {.ProductName = "Ipoh Coffee", .UnitPrice = 36.8, .UnitsOnOrder = 12})
+                data.Add(New Product() With {.ProductName = "Outback Lager", .UnitPrice = 12, .UnitsOnOrder = 25})
+                data.Add(New Product() With {.ProductName = "Boston Crab Meat", .UnitPrice = 18.4, .UnitsOnOrder = 18})
+                data.Add(New Product() With {.ProductName = "Konbu", .UnitPrice = 6, .UnitsOnOrder = 24})
+                Return data
+            End Function
+        End Class
+        Public Class Product
+            Public Property ProductName() As String
+            Public Property UnitPrice() As Double
+            Public Property UnitsOnOrder() As Integer
+        End Class
+    End Class
+End Namespace
